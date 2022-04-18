0f761b13339698e5b82a7158cb1dc361199a364d
Valdemarick <korolev.vadim.2017@mail.ru>
2022-04-18 20:26:30 +0300

    created board component

---
+++ src/Components/Board.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Square from "./Square";
+
+class Board extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        };
+    }
+
+    handleClick(i){
+        if (this.state.squares[i]){
+            return;
+        }
+        
+        const copy = this.state.squares.slice();
+        copy[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({squares: copy, xIsNext: !this.state.xIsNext});
+    }
+
+    renderSquare(i){
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+    }
+
+    render(){
+        const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+
+        return(
+            <div>   
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Board;
