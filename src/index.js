f4498196f6e6f823185128f7134e97d870e43543
Valdemarick <korolev.vadim.2017@mail.ru>
2022-04-18 20:30:20 +0300

    removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; 
-import App from './App';
+import Game from './Components/Game';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <Game />
 );
